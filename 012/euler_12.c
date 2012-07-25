bb31be03bb7fa0bcced434a28d3cf749f6114d08
Kevin D <kevin@kads24.eu>
2012-07-25 16:04:36 +0200

    Solution for problem 12.

--- 012/euler_12.c
+++ 012/euler_12.c
@@ -1,69 +1,38 @@
-#include "stdio.h"
-#include "stdlib.h"
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int *map_sieve(int N) {
-	int i, j;
-	int *array;
-	array = malloc(sizeof(int)*(N+1));
-	if (!array) {
-		printf("Error: Could not allocate memory.\n");
-		return 0;
-	}
+int num_divisors(int x) {
+	int i, sum, sq;
 
-	/* Setup array */
-	for (i=0; i<=N; i++) {
-		array[i] = i;
-	}
-
-	/* Run the sieve */
-	for (i=2; i<=N/2; i++) {
-		for (j=2*i; j<=N; j+=i) {
-			array[j] /= array[i];
+	sum = 0;
+	sq = (int) sqrt(x);
+	
+	for (i=1; i<sq; i++) {
+		if (x%i == 0) {
+			sum += 2;
 		}
 	}
 
-	return array;
-}
-
-long product(int *array, int N) {
-	int i;
-	long p;
-	p = 1L;
-	for (i=2; i<=N; i++) {
-		p *= array[i];
+	if(x%sq == 0) {
+		sum++;
 	}
 
-	return p;
-}
-
-int print_array(int *array, int N) {
-	int i;
-	if (!array || N<1) {
-		return 0;
-	}
-	printf("{\n");
-	for (i=0; i<=N; i++) {
-		printf("\t%d: %d\n", i, array[i]);
-	}
-	printf("}\n");
-	return 1;
-}
-
-long reduced_factorial(int N) {
-	int *sieve;
-	long p;
-	sieve = map_sieve(N);
-	p =  product(sieve, N);
-	free(sieve);
-	return p;
+	return sum;
 }
 
 int main(int argc, char *argv[]) {
-	int *sieve, N;
-	N = 500;
-	sieve = map_sieve(N);
-	print_array(sieve, N);
-	printf("%dr! = %ld\n", N, reduced_factorial(N));
+	int t, n;
+
+	t = 1;
+	n = 1;
+
+	while(num_divisors(t) <= 500) {
+		n++;
+		t = n*(n+1)/2;
+	}
+
+	printf("%d\n", t);
 
 	return EXIT_SUCCESS;
 }
