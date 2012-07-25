4e95676024c83e00714c787966445e01644cc48b
Kevin D <kevin@kads24.eu>
2012-07-25 11:01:21 +0200

    Added functional version for problem 13.

--- 013/euler_13.py
+++ 013/euler_13.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python
 
+from operator import add
+
+with open('numbers.txt') as numfile:
+	_sum = reduce(add, map(int, numfile.readlines()))
+	print(_sum)
+
+
+"""
+# This was my first idea, but I prefer the functional version...
 _sum = 0
 with open('numbers.txt') as numfile:
 	for line in numfile.readlines():
-		x = int(line.strip('\n'))
+		x = int(line)
 		_sum += x
 
 print(_sum)
+"""
