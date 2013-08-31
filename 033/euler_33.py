f150e9db94f70759a62a37142c92d9c72ad84b52
Kevin Dungs <kevin@kads24.eu>
2013-08-31 18:39:22 +0200

    Solve problem 33.

---
+++ 033/euler_33.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from fractions import Fraction, gcd
+from functools import reduce
+from operator import mul
+
+
+result = []
+for b in range(1, 10):
+    for a in range(1, b):
+        frac = a / b
+        for c in range(1, 10):
+            _1 = (a * 10 + c) / (b * 10 + c)
+            _2 = (a * 10 + c) / (b + 10 * c)
+            _3 = (a + 10 * c) / (b * 10 + c)
+            _4 = (a + 10 * c) / (b + 10 * c)
+            if frac == _1 or frac == _2 or frac == _3 or frac == _4:
+                result.append(Fraction(a, b))
+
+
+print(reduce(mul, result))
