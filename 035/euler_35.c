314faddb33603a433a56c5641794b6f22a04338b
Kevin Dungs <kevin@kads24.eu>
2013-09-05 22:37:51 +0200

    Solve 35 in C.

---
+++ 035/euler_35.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "../tools/prime_tools.h"
+
+int rotate(int x) {
+    int pow = 1,
+        tmp = x / 10;
+    while (tmp > 0) {
+        pow *= 10;
+        tmp /= 10;
+    }
+    return x / 10 + (x % 10) * pow;
+}
+
+
+int main(int argc, char *argv[]) {
+    int N = 1000000,
+        num = 0;
+    PT *table = pt_create(N);
+    pt_destroy(table);
+
+    int p = 2;
+    while (p) {
+        int is_circ = 1,
+            rot = rotate(p);
+        while (p != rot) {
+            if (!is_prime(rot, table)) {
+                is_circ = 0;
+                break;
+            }
+            rot = rotate(rot);
+        }
+        if (is_circ) {
+            num++;
+        }
+        p = next_prime(p, table);
+    }
+
+    printf("%d\n", num);
+}
