cac9b3996d5a5926026182c6495758d967aa4981
Kevin Dungs <kevin@kads24.eu>
2013-09-13 13:31:38 +0200

    Solve 38 in Python

--- 038/euler_38.py
+++ 038/euler_38.py
@@ -1,29 +1,34 @@
 #!/usr/bin/env python
 
+from itertools import chain, starmap
+
+
 def is_pandigital(s):
     return len(s) == 9 and all(s.count(c) == 1 for c in '123456789')
 
 
-if __name__ == '__main__':
-    from itertools import product
-    combs = [
-        (1, 9), (1, 8), (1, 7), (1, 6), (1, 5),
-        (2, 3), (2, 4),
-        (3, 3),
-        (4, 2)
-    ]
-
-    pandigital = set()
-    digits = (1, 2, 3, 4, 5, 6, 7, 8, 9)
-    for outer, inner in combs:
-        nums = (i for i in range(10 ** (outer - 1), 10 ** outer))
-        mult = (digits[i:(i + inner)] for i in range(10 - inner))
-        pandigital.update(list(filter(
-            is_pandigital,
-            (''.join(map(
+def rangeproduct(a, b):
+    return (
+        ''.join(
+            map(
                 str,
-                (num * m for m in mul))) for num, mul in product(nums, mult)
+                (_r * d for d in range(1, b + 1))
             )
-        )))
-    print(max(pandigital))
-    print(pandigital)
+        ) for _r in range(10 ** (a - 1), 10 ** a)
+    )
+
+
+combs = [
+    (1, 9), (1, 8), (1, 7), (1, 6), (1, 5),
+    (2, 3), (2, 4),
+    (3, 3),
+    (4, 2)
+]
+
+result = max(
+    filter(
+        is_pandigital,
+        chain(*starmap(rangeproduct, combs))
+    )
+)
+print(result)
